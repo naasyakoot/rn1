b10fa857e1f8f35c456b04427749be51d9147513
Tester <seumworldinbangladesh@gmail.com>
2020-12-12 02:26:31 +0600

    test1 style outline fontSize  apply  done

--- App.js
+++ App.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
 
-import { View,Text } from 'react-native';
+import { View,Text,StyleSheet } from 'react-native';
 
 class App extends Component {
   render() {
+    
+
     return (
       <View>
-        <Text style={{color:'green',fontSize:24}}> SALAM this is my First Component With React Native  </Text>
+        <Text style={myStyle.green}> SALAM this is my First Component With React Native  </Text>
       </View>
     );
+
+    const myStyle = StyleSheet.create({
+      green: {
+        color: 'green',
+        fontSize: 24
+      }
+    });
+
+    
   }
+
+  
 }
 
 export default App;
